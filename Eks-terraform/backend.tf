a16292a20c9ece7b5f3e446674fb5d9bfa58c8f6
Avinash <138698917+maximusdecimusmeridiu@users.noreply.github.com>
2025-05-14 12:00:56 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "avinash-14may2003" # Replace with your actual S3 bucket name
+    bucket = "avinash-14may-2003" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
